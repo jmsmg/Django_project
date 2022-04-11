7a9b23a2e859cc102cc1bd18af63cf056e45bb12
jmsmg <jmsmg1@me.com>
2022-04-12 03:03:24 +0900

    예외처리

--- instagram/urls.py
+++ instagram/urls.py
@@ -17,7 +17,7 @@ app_name = 'instagram' # name space 지정
 
 urlpatterns = [
     path('', views.post_list),
-    # path('<int:pk>/', views.post_detail),
+    path('<int:pk>/', views.post_detail),
     # path('archives/<int:year>/', views.archives_year),
     path('archives/<year:year>/', views.archives_year)
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 
@@ -18,7 +18,16 @@ post_list = ListView.as_view(model=Post) # 아래 포스트 리스트 전체 읽
 #         'q': q,        
 #     })
 
-def post_detail(request:HttpRequest, pk:int) -> HttpResponse:
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    post = get_object_or_404.get(Post, pk=pk) # deosnotexist 예외
+    # try:
+    #     post = Post.objects.get(pk=pk) # deosnotexist 예외
+    # except Post.DoesNotExist:
+    #     raise Http404
+    return render(request, 'instagram/post_detail.html', {
+        'post': post,
+    })
+
     response = HttpResponse()
     response.write("Hello World")
     return render(request, 'instagram/post_list.html', {})
