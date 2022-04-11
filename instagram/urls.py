bc5b24e2747706f8da7196fe0772f1f2f1918ce0
jmsmg <jmsmg1@me.com>
2022-04-12 02:17:33 +0900

    디스패쳐, 정규표현식까지

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,7 +1,23 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 
 from . import views
 
+class YearConverter:
+    regex = r'20\d{2}'
+
+    def to_python(self, value):
+        return int(value)
+    
+    def to_url(self, value):
+        return str(value)
+
+register_converter(YearConverter, 'year')
+
+app_name = 'instagram' # name space 지정
+
 urlpatterns = [
-    path('', views.post_list)
+    path('', views.post_list),
+    # path('<int:pk>/', views.post_detail),
+    # path('archives/<int:year>/', views.archives_year),
+    path('archives/<year:year>/', views.archives_year)
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,16 +1,27 @@
+from django.views.generic import ListView
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from .models import Post
 # Create your views here.
 
+post_list = ListView.as_view(model=Post) # 아래 포스트 리스트 전체 읽어오기 가능하되 검색은 안됨
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q', '')
-    if q:
-        qs = qs.filter(message__icontains=q)
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q', '')
+#     if q:
+#         qs = qs.filter(message__icontains=q)
 
-    # instagram/templates/instagram/post_list.html
-    return render(request, 'instagram/post_list.html', {
-        'post_list': qs,
-        'q': q,        
-    })
+#     # instagram/templates/instagram/post_list.html
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list': qs,
+#         'q': q,        
+#     })
+
+def post_detail(request:HttpRequest, pk:int) -> HttpResponse:
+    response = HttpResponse()
+    response.write("Hello World")
+    return render(request, 'instagram/post_list.html', {})
+
+def archives_year(request, year):
+    return HttpResponse(f'{year}년 arhcive')
