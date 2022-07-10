9f3b72709f2129d81fd1acc09ebe3810a930a9e4
jmsmg <jmsmg1@me.com>
2022-07-10 23:44:16 +0900

    one to one

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,7 @@ from django.db import models
 # Create your models here.
 
 class Profile(models.Model):
+    # user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     address = models.CharField(max_length=100)
     zipcode = models.CharField(max_length=6) # validators=[]
