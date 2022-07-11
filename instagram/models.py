f8431ba7689fc9d063e118a086ca792ebd77c60e
jmsmg <jmsmg1@me.com>
2022-07-11 23:33:15 +0900

    MTM

--- instagram/models.py
+++ instagram/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
     photo = models.ImageField(blank=True, upload_to='instagram/post/%Y/%m/%d') # 1폴더에 파일이 몰리는 것을 방지 uload_to
-    tag_set = models.ManyToManyField('Tag', blank=True)
+    tag_set = models.ManyToManyField('Tag', blank=True) # 'Tag'로 지정하는 이유는 class Tag가 아직 읽혀지지 않았기 때문, MTM blank True로 하는 것이 좋음
     is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -38,7 +38,8 @@ class Comment(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, unique=True) # 테이블에서 unique 함을 보장 받도록 함
+    # post_set = models.ManyToManyField(Post)
 
     def __str__(self):
         return self.name
